224fa47b4b87b9bbc1bae37ee230628113e3434f
Henry <67548612+NurHenry@users.noreply.github.com>
2023-09-01 18:11:18 +0200

    Update main.py
    Removed stuff

--- main.py
+++ main.py
@@ -1,31 +1,20 @@
 import cv2
 
-# Create a haar cascade classifier
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-# Start Webcam
 cap = cv2.VideoCapture(0)
 
 while True:
-    # Read Frames
     _, frame = cap.read()
-
-    # Konvert Frames
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Face Tracking
     faces = face_cascade.detectMultiScale(gray)
-
-    # Create Circel around the Face
+    
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
-    # Shows the Frame
+        
     cv2.imshow('Dgesichtserkennung', frame)
 
-    # Press Q to end
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 
 cap.release()
